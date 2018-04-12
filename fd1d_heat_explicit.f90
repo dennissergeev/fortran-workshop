8634a52ba5433ccc3f9f0c7d9b4bdade633c15b8
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-04-12 15:55:36 +0100

    clean up the code following the exercises

--- fd1d_heat_explicit.f90
+++ fd1d_heat_explicit.f90
@@ -1,28 +1,39 @@
 program fd1d_heat_explicit_prb
-  use :: types_mod, only: dp
+  use :: types_mod, only: DP
 
   implicit none
 
   integer, parameter :: T_NUM = 201
   integer, parameter :: X_NUM = 21
 
-  real (kind=dp) :: cfl
-  real (kind=dp) :: dt
-  real (kind=dp) :: h(X_NUM)
-  real (kind=dp) :: h_new(X_NUM)
+  real (kind=DP)                  :: cfl
+  real (kind=DP)                  :: dt
+  real (kind=DP), allocatable, dimension(:)    :: h
+  real (kind=DP), allocatable, dimension(:)    :: h_new
 ! the "matrix" stores all x-values for all t-values
 ! remember Fortran is column major, meaning that rows are contiguous
-  real (kind=dp) :: hmat(X_NUM, T_NUM)
-  integer :: i
-  integer :: j
-  real (kind=dp) :: k
+  real (kind=DP), allocatable, dimension(:, :) :: hmat
+  integer                         :: i
+  integer                         :: j
+  real (kind=DP)                  :: k
 
-  real (kind=dp) :: t(T_NUM)
-  real (kind=dp) :: t_max
-  real (kind=dp) :: t_min
-  real (kind=dp) :: x(X_NUM)
-  real (kind=dp) :: x_max
-  real (kind=dp) :: x_min
+  real (kind=DP), allocatable, dimension(:)    :: t
+  real (kind=DP)                  :: t_max
+  real (kind=DP)                  :: t_min
+  real (kind=DP), allocatable, dimension(:)    :: x
+  real (kind=DP)                  :: x_max
+  real (kind=DP)                  :: x_min
+  integer                         :: ierr 
+
+
+  allocate(h    (1:X_NUM         ), stat=ierr)
+  allocate(h_new(1:X_NUM         ), stat=ierr)
+  allocate(hmat (1:X_NUM, 1:T_NUM), stat=ierr)
+  allocate(t    (         1:T_NUM), stat=ierr)
+  allocate(x    (1:X_NUM         ), stat=ierr)
+  if ( ierr /= 0 ) then
+    write(*, '(a)') 'Failed to allocate arrays'
+  end if
 
   write (*, '(a)') ' '
   write (*, '(a)') 'FD1D_HEAT_EXPLICIT_PRB:'
@@ -50,15 +61,15 @@ program fd1d_heat_explicit_prb
   x_min = 0.0e+00_dp
   x_max = 1.0e+00_dp
 ! X_NUM is the number of intervals in the x-direction
-  call r8vec_linspace(X_NUM, x_min, x_max, x)
+  call r8vec_linspace(x_min, x_max, x)
 
 ! the t-range values. integrate from t_min to t_max
   t_min = 0.0e+00_dp
   t_max = 80.0e+00_dp
 
 ! T_NUM is the number of intervals in the t-direction
-  dt = (t_max-t_min)/real(T_NUM-1, kind=dp)
-  call r8vec_linspace(T_NUM, t_min, t_max, t)
+  dt = (t_max-t_min)/real(T_NUM-1, kind=DP)
+  call r8vec_linspace(t_min, t_max, t)
 
 ! get the CFL coefficient
   call fd1d_heat_explicit_cfl(k, T_NUM, t_min, t_max, X_NUM, x_min, x_max, &
@@ -88,7 +99,8 @@ program fd1d_heat_explicit_prb
 
 ! the main time integration loop 
   do j = 2, T_NUM
-    call fd1d_heat_explicit(X_NUM, x, t(j-1), dt, cfl, h, h_new)
+    call fd1d_heat_explicit(x, dt, cfl, h, h_new)
+    ! call fd1d_heat_explicit(x, t(j-1), dt, cfl, h, h_new)
 
     do i = 1, X_NUM
       hmat(i, j) = h_new(i)
@@ -97,44 +109,51 @@ program fd1d_heat_explicit_prb
   end do
 
 ! write data to files
-  call r8mat_write('h_test01.txt', X_NUM, T_NUM, hmat)
-  call r8vec_write('t_test01.txt', T_NUM, t)
-  call r8vec_write('x_test01.txt', X_NUM, x)
+  call r8mat_write('h_test01.txt', hmat)
+  call r8vec_write('t_test01.txt', t)
+  call r8vec_write('x_test01.txt', x)
+
+  deallocate(h    )
+  deallocate(h_new)
+  deallocate(hmat )
+  deallocate(t    )
+  deallocate(x    )
 
 contains
 
-  function func(j, X_NUM, x) result (d)
+  function func() result (d)
     implicit none
 
-    integer :: j, X_NUM
-    real (kind=dp) :: d
-    real (kind=dp) :: x(X_NUM)
+    ! integer :: j
+    ! real (kind=DP), dimension(:) :: x
+    real (kind=DP) :: d
 
     d = 0.0e+00_dp
   end function
 
-  subroutine fd1d_heat_explicit(X_NUM, x, t, dt, cfl, h, h_new)
+  subroutine fd1d_heat_explicit(x, dt, cfl, h, h_new)
     implicit none
 
-    integer :: X_NUM
-
-    real (kind=dp), intent (in) :: cfl
-    real (kind=dp), intent (in) :: dt
-    real (kind=dp), intent (in) :: h(X_NUM)
-    real (kind=dp), intent (in) :: t
-    real (kind=dp), intent (in) :: x(X_NUM)
-    real (kind=dp), intent (out) :: h_new(X_NUM)
+    real (kind=DP), intent (in) :: cfl
+    real (kind=DP), intent (in) :: dt
+    ! real (kind=DP), intent (in) :: t
+    real (kind=DP), dimension(:), intent (in)  :: h
+    real (kind=DP), dimension(:), intent (in)  :: x
+    real (kind=DP), dimension(:), intent (out) :: h_new
 
     integer :: j
-    real (kind=dp) :: f(X_NUM)
+    real (kind=DP) :: f(size(x))
+    integer :: xn
 
-    do j = 1, X_NUM
-      f(j) = func(j, X_NUM, x)
+    xn = size(x, 1)
+
+    do j = 1, xn
+      f(j) = func()
     end do
 
     h_new(1) = 0.0e+00_dp
 
-    do j = 2, X_NUM - 1
+    do j = 2, xn - 1
       h_new(j) = h(j) + dt*f(j) + cfl*(h(j-1)-2.0e+00_dp*h(j)+h(j+1))
     end do
 
@@ -150,17 +169,17 @@ contains
 
     integer :: T_NUM
     integer :: X_NUM
-    real (kind=dp), intent (in) :: k
-    real (kind=dp), intent (in) :: t_max
-    real (kind=dp), intent (in) :: t_min
-    real (kind=dp), intent (in) :: x_max
-    real (kind=dp), intent (in) :: x_min
-    real (kind=dp), intent (out) :: cfl
-    real (kind=dp) :: dx
-    real (kind=dp) :: dt
+    real (kind=DP), intent (in) :: k
+    real (kind=DP), intent (in) :: t_max
+    real (kind=DP), intent (in) :: t_min
+    real (kind=DP), intent (in) :: x_max
+    real (kind=DP), intent (in) :: x_min
+    real (kind=DP), intent (out) :: cfl
+    real (kind=DP) :: dx
+    real (kind=DP) :: dt
 
-    dx = (x_max-x_min)/real(X_NUM-1, kind=dp)
-    dt = (t_max-t_min)/real(T_NUM-1, kind=dp)
+    dx = (x_max-x_min)/real(X_NUM-1, kind=DP)
+    dt = (t_max-t_min)/real(T_NUM-1, kind=DP)
 
     cfl = k*dt/dx/dx
 
@@ -169,16 +188,19 @@ contains
 
   end subroutine
 
-  subroutine r8mat_write(output_filename, m, n, table)
+  subroutine r8mat_write(output_filename, table)
     implicit none
 
-    integer,           intent(in) :: m
-    integer,           intent(in) :: n
     character (len=*), intent(in) :: output_filename
-    real (kind=dp)   , intent(in) :: table(m, n)
+    real (kind=DP)   , dimension(:, :), intent(in) :: table
+    integer :: m
+    integer :: n
     integer :: j
     integer :: output_unit_id
     character (len=30) :: string
+
+    m = size(table, 1)
+    n = size(table, 2)
 
     output_unit_id = 10
     open (unit=output_unit_id, file=output_filename, status='replace')
@@ -192,37 +214,38 @@ contains
     close (unit=output_unit_id)
   end subroutine
 
-  subroutine r8vec_linspace(n, a_first, a_last, a)
+  subroutine r8vec_linspace(a_first, a_last, a)
 
     implicit none
 
-    integer       , intent(in)  :: n
-    real (kind=dp), intent(in)  :: a_first
-    real (kind=dp), intent(in)  :: a_last
-    real (kind=dp), intent(out) :: a(n)
+    real (kind=DP), intent(in)  :: a_first
+    real (kind=DP), intent(in)  :: a_last
+    real (kind=DP), dimension(:), intent(out) :: a
     integer :: i
+    integer :: n
+
+    n = size(a, 1)
 
     do i = 1, n
-      a(i) = (real(n-i,kind=dp)*a_first+real(i-1,kind=dp)*a_last)/ &
-        real(n-1, kind=dp)
+      a(i) = (real(n-i,kind=DP)*a_first+real(i-1,kind=DP)*a_last)/ &
+        real(n-1, kind=DP)
     end do
 
   end subroutine
 
-  subroutine r8vec_write(output_filename, n, x)
+  subroutine r8vec_write(output_filename, x)
 
     implicit none
 
-    integer          , intent(in) :: n
     character (len=*), intent(in) :: output_filename
-    real (kind=dp)   , intent(in) :: x(n)
+    real (kind=DP)   , dimension(:), intent(in) :: x
     integer :: output_unit_id
     integer :: j
 
     output_unit_id = 11
     open (unit=output_unit_id, file=output_filename, status='replace')
 
-    do j = 1, n
+    do j = 1, size(x, 1)
       write (output_unit_id, '(2x,g24.16)') x(j)
     end do
 
